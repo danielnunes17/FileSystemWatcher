f57a37c5bf06cc57f1976de15590a53160eb2195
Daniel Nunes <danielnunes17@gmail.com>
2022-02-28 15:41:47 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿var path = @"C:\Work\C#\streams\directry_and_directory_info\Globo";
+using var fsw = new FileSystemWatcher(path);
+fsw.Created += OnCreated;
+fsw.Deleted += OnDeleted;
+fsw.Renamed += OnRenamed;
+
+fsw.EnableRaisingEvents = true;
+fsw.IncludeSubdirectories = true;
+
+Console.WriteLine("Pressione [ENTER] para finalizar");
+Console.ReadLine();
+
+void OnCreated(object sender, FileSystemEventArgs e)
+{
+     Console.WriteLine($"O arquivo foi ciado{e.Name}");
+}
+
+void OnDeleted(object sender, FileSystemEventArgs e)
+{
+    Console.WriteLine($"O arquivo foi excluido{e.Name}");
+}
+
+
+void OnRenamed(object sender, RenamedEventArgs e)
+{
+    Console.WriteLine($"O arquivo {e.OldName} foi renomeado para {e.Name}");
+}
